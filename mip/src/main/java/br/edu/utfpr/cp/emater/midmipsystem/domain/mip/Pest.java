40fd5aff0074148e181578ba9a9ce4882193a02a
Gabriel Costa Silva <gabriel@Gabriels-iMac.local>
2019-03-30 21:25:23 -0300

    - Released Pest Management functionality;

--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/domain/mip/Pest.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/domain/mip/Pest.java
@@ -7,36 +7,52 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 import lombok.Builder;
+import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
 
-@Getter
-@EqualsAndHashCode (onlyExplicitlyIncluded = true)
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Pest {
 
     public enum PestSize {
-        GREATER_15CM,
-        SMALLER_15CM,
-        THIRD_TO_FIFTH_INSTAR,
-        ADULT;
+        GREATER_15CM("> 15 cm"), 
+        SMALLER_15CM("< 15 cm"), 
+        THIRD_TO_FIFTH_INSTAR("3. ao 5. Instar"), 
+        ADULT("Adultos");
+
+        private String name;
+
+        PestSize(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 
     @EqualsAndHashCode.Include
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
     private String usualName;
 
     @EqualsAndHashCode.Include
     private String scientificName;
 
-    @Enumerated (EnumType.STRING)
+    @Enumerated(EnumType.STRING)
     private PestSize pestSize;
 
-    private Pest() {}
+    private Pest() {
+    }
 
     @Builder
-    public static Pest create (String usualName, String scientificName, PestSize pestSize) {
+    public static Pest create(String usualName, String scientificName, PestSize pestSize) {
         Pest instance = new Pest();
         instance.usualName = usualName;
         instance.scientificName = scientificName;
--- mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/controller/PestController.java
+++ mip/src/main/java/br/edu/utfpr/cp/emater/midmipsystem/view/mip/controller/PestController.java
@@ -11,84 +11,80 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-//@Controller
-//@RequestMapping (value = "/pest")
+@Controller
+@RequestMapping (value = "/pest")
 public class PestController {
     
-//    private final PestRepository repository;
-//
-//    private final Environment environment;
-//
-//    private boolean operationSuccessMessage;
-//
-//    private void resetOperationSuccessMessage() {
-//        if (this.operationSuccessMessage)
-//            this.operationSuccessMessage = false;
-//    }
-//
-//    private void setOperationSuccessMessage() {
-//        this.operationSuccessMessage = true;
-//    }
-//
-//
-//    @Autowired
-//    public PestController(PestRepository repository, Environment environment) {
-//        this.repository = repository;
-//        this.environment = environment;
-//        this.operationSuccessMessage = false;
-//    }
-//    
-//    @RequestMapping (value = "", method = RequestMethod.GET)
-//    public String listAll(Model data) {
-//        data.addAttribute("pests", repository.findAll());
-//        data.addAttribute("pestSizes", PestSize.values());
-//        data.addAttribute("success", this.operationSuccessMessage);
-//
-//        this.resetOperationSuccessMessage();
-//        
-//        data.addAttribute("urlCreate", this.environment.getProperty("app.view.route.create.mip.pest"));
-//        data.addAttribute("urlUpdate", this.environment.getProperty("app.view.route.update.mip.pest"));
-//        data.addAttribute("urlDelete", this.environment.getProperty("app.view.route.delete.mip.pest"));
-//        
-//        return this.environment.getProperty("app.view.route.template.main.mip.pest");
-//    }
-//    
-//    @RequestMapping (value = "/create", method = RequestMethod.POST)
-//    public String create (PestDTO pest) {
-//        
-//        Pest p = new Pest();
-//        p.setScientificName(pest.getScientificName());
-//        p.setUsualName(pest.getUsualName());
-//        p.setPestSize(PestSize.valueOf(pest.getPestSize()));
-//
-//        repository.save(p);
-//
-//        this.setOperationSuccessMessage();
-//        
-//        return this.environment.getProperty("app.view.route.create.success.mip.pest");
-//    }
-//    
-//    @RequestMapping (value = "/update", method = RequestMethod.POST)
-//    public String update (PestDTO pest) {
-//
-//        Pest originalPest = repository.findById(new Long(pest.getId())).orElseThrow();
-//        originalPest.setUsualName(pest.getUsualName());
-//        originalPest.setScientificName(pest.getScientificName());
-//        originalPest.setPestSize(PestSize.valueOf(pest.getPestSize()));
-//        
-//        repository.saveAndFlush(originalPest);
-//
-//        this.setOperationSuccessMessage();
-//        
-//        return this.environment.getProperty("app.view.route.update.success.mip.pest");
-//    }
-//
-//    @RequestMapping (value = "/delete", method = RequestMethod.POST)
-//    public String delete (@RequestParam int id) {
-//        repository.deleteById(new Long(id));
-//
-//        this.setOperationSuccessMessage();
-//        
-//        return this.environment.getProperty("app.view.route.delete.success.mip.pest");
-//    }
+   private final PestRepository repository;
+
+   private final Environment environment;
+
+   private boolean operationSuccessMessage;
+
+   private void resetOperationSuccessMessage() {
+       if (this.operationSuccessMessage)
+           this.operationSuccessMessage = false;
+   }
+
+   private void setOperationSuccessMessage() {
+       this.operationSuccessMessage = true;
+   }
+
+   @Autowired
+   public PestController(PestRepository repository, Environment environment) {
+       this.repository = repository;
+       this.environment = environment;
+       this.operationSuccessMessage = false;
+   }
+   
+   @RequestMapping (value = "", method = RequestMethod.GET)
+   public String listAll(Model data) {
+       data.addAttribute("pests", repository.findAll());
+       data.addAttribute("pestSizes", Pest.PestSize.values());
+       data.addAttribute("success", this.operationSuccessMessage);
+
+       this.resetOperationSuccessMessage();
+       
+       data.addAttribute("urlCreate", this.environment.getProperty("app.view.route.create.mip.pest"));
+       data.addAttribute("urlUpdate", this.environment.getProperty("app.view.route.update.mip.pest"));
+       data.addAttribute("urlDelete", this.environment.getProperty("app.view.route.delete.mip.pest"));
+       
+       return this.environment.getProperty("app.view.route.template.main.mip.pest");
+   }
+   
+   @RequestMapping (value = "/create", method = RequestMethod.POST)
+   public String create (PestDTO pest) {
+       
+       Pest pestInstance = Pest.builder().scientificName(pest.getScientificName()).usualName(pest.getUsualName()).pestSize(Pest.PestSize.valueOf(pest.getPestSize())).build();
+
+       repository.save(pestInstance);
+
+       this.setOperationSuccessMessage();
+       
+       return this.environment.getProperty("app.view.route.create.success.mip.pest");
+   }
+   
+   @RequestMapping (value = "/update", method = RequestMethod.POST)
+   public String update (PestDTO pest) {
+
+       Pest originalPest = repository.findById(new Long(pest.getId())).orElseThrow();
+       originalPest.setUsualName(pest.getUsualName());
+       originalPest.setScientificName(pest.getScientificName());
+       originalPest.setPestSize(Pest.PestSize.valueOf(pest.getPestSize()));
+       
+       repository.saveAndFlush(originalPest);
+
+       this.setOperationSuccessMessage();
+       
+       return this.environment.getProperty("app.view.route.update.success.mip.pest");
+   }
+
+   @RequestMapping (value = "/delete", method = RequestMethod.POST)
+   public String delete (@RequestParam int id) {
+       repository.deleteById(new Long(id));
+
+       this.setOperationSuccessMessage();
+       
+       return this.environment.getProperty("app.view.route.delete.success.mip.pest");
+   }
 }
